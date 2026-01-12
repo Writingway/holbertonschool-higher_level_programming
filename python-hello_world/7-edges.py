085389f0239089cda60a92d8265cde2edc0c1b3e
Writingway <sendmeat4time@gmail.com>
2026-01-12 16:22:12 +0100

    feat: add script to print segments of the word "Holberton"

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+
+
+# Print the first 3 letters, last 2 letters, and the middle word
+print(f"First 3 letters: {word[:3]}")
+print(f"Last 2 letters: {word[7:]}")
+print(f"Middle word: {word[1:8]}")
