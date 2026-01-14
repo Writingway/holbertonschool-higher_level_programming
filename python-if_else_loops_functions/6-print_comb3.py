92d640411e9cd2fb7c23580765f4aa59ff36cc4d
Writingway <sendmeat4time@gmail.com>
2026-01-14 15:45:24 +0100

    fix: simplify string formatting in combination printing script

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 9):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
-            print(("{:01}{:01}").format(i, j), end="\n")
+            print(("{}{}").format(i, j), end="\n")
         else:
-            print(("{:01}{:01}").format(i, j), end=", ")
+            print(("{}{}").format(i, j), end=", ")
