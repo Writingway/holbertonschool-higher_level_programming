70fe1af59ea6d45c03cbb524ea59c2512d8d9b87
Writingway <sendmeat4time@gmail.com>
2026-01-14 10:28:48 +0100

    feat: add last digit checker with random number generation

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 5:
+    print(f"Last digit of {number} and is greater than 5")
+if number == 0:
+    print(f"Last digit of {number} and is 0")
+if number < 6 & number > 0:
+    print(f"Last digit of {number} and is less than 6 and not 0")
