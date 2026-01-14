22f380b214fb263cd9b3342f7d7fb17bf88f9c70
Writingway <sendmeat4time@gmail.com>
2026-01-14 10:32:00 +0100

    fix: correct output message formatting in last digit checker

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = int(repr(number)[-1])
 if number > 5:
-    print(f"Last digit of {number} is {last_digit} is and is greater than 5")
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
 if number == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 if number < 6 & number > 0:
