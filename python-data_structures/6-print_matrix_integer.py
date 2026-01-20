fab8d128edb39f9f11a517739fd5d33208aafedd
Writingway <sendmeat4time@gmail.com>
2026-01-20 15:06:24 +0100

    fix: add row length check in print_matrix_integer to prevent index error

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for col in range(len(matrix[row])):
-            print("{:d}".format(matrix[row][col]), end=" ")
+            if row < len(matrix):
+                print("{:d}".format(matrix[row][col]), end=" ")
         print()
