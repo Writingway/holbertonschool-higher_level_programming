491a9efcfa38938161fc34bab30604df2bfbd35a
Writingway <sendmeat4time@gmail.com>
2026-01-20 15:02:11 +0100

    feat: implement print_matrix_integer function to print a matrix of integers

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            print(matrix[row][col], end=" ")
+        print()
