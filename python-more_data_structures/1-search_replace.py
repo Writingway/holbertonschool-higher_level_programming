6f88135d6a29aad1c5ebe273aa70d47703a7dbdf
Writingway <sendmeat4time@gmail.com>
2026-01-22 11:44:40 +0100

    fix: update condition in search_replace function to use search parameter

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -5,6 +5,6 @@ def search_replace(my_list, search, replace):
     new_list += my_list
 
     for idx, item in enumerate(new_list):
-        if item == 2:
+        if item == search:
             new_list[idx] = replace
     return (new_list)
