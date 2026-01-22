b97e34e7d56f2c7bd7ca31b99d86a8895e074d04
Writingway <sendmeat4time@gmail.com>
2026-01-22 11:43:28 +0100

    feat: implement search_replace function to replace elements in a list

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = []
+    new_list += my_list
+
+    for idx, item in enumerate(new_list):
+        if item == 2:
+            new_list[idx] = replace
+    return (new_list)
