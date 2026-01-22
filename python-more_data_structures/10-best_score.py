f54b3b80e435598dba2620a85ce30ecb64e611b8
Writingway <sendmeat4time@gmail.com>
2026-01-22 18:36:16 +0100

    fix: correct max function key argument in best_score to use a_dictionary.get

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
-    result = max(a_dictionary, key=a_dictionary.get())
+    result = max(a_dictionary, key=a_dictionary.get)
     return (result)
