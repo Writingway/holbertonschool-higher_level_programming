98e0e29b8c7c349858b1a9106d103f778872800e
Writingway <sendmeat4time@gmail.com>
2026-01-28 10:07:08 +0100

    feat: implement Square class with initialization, area calculation, and print method

---
+++ python-classes/5-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """
+    Docstring for Square class.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes the square with a given size.
+        Args:
+        size (int): The size of the square's sides.
+        Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is negative.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square.
+        Returns:
+            int: The area of the square.
+        """
+        square_area = self.__size * self.__size
+        return square_area
+
+    @property
+    def size(self):
+        """
+        Calculates the area of the square.
+        Returns:
+            int: The size of the square's sides.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the area of the square.
+        Args:
+            value (int): The new size of the square's sides.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is negative.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """
+        Docstring for my_print
+
+        :param self: Description
+        """
+        if self.__size == 0:
+            print("")
+            return
+        for _ in range(self.__size):
+            print("#" * self.__size)
