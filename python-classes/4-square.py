7161f877797ec5bd4ea9a23f9e81ed6c580b315f
Writingway <sendmeat4time@gmail.com>
2026-01-28 09:21:07 +0100

    refactor: update module docstring for Square class to include module description

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
 class Square:
     """
     Docstring for Square class.
-    Represents a square with height and width attributes.
     """
     def __init__(self, size=0):
         """
