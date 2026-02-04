ffcb09014b4c67b83869178a4f57ffcecaa66f84
Writingway <sendmeat4time@gmail.com>
2026-02-04 11:48:39 +0100

    feat: add Animal abstract class with Dog and Cat implementations

---
+++ python-abc/README.md
@@ -0,0 +1,21 @@
+# Python – Abstract Classes and Interfaces
+
+## Description
+
+Projet Python consacré aux classes abstraites et aux interfaces à l’aide du module `abc`, afin de définir des comportements communs et imposer des méthodes aux classes dérivées.
+
+## Objectifs
+
+* Comprendre les classes abstraites (`ABC`)
+* Utiliser les méthodes abstraites (`@abstractmethod`)
+* Structurer une architecture orientée objet propre
+
+## Technologies
+
+* Python 3
+
+## Exécution
+
+```bash
+python3 nom_du_fichier.py
+```
---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This module defines an abstract class 'Animal' with an abstract method 'sound'.
+"""
+
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """
+    Abstract class representing an animal.
+    """
+
+    @abstractmethod
+    def sound(self):
+        """
+        Abstract method to be implemented by subclasses to return 
+        the sound made by the animal.
+        """
+        pass
+
+
+class Dog(Animal):
+    """
+    Represents a dog, inherits from Animal
+    """
+    def sound(self):
+        """
+        Returns the sound made by a dog.
+        """
+        return "Bark"
+
+
+class Cat(Animal):
+    """
+    Represents a cat, inherits from Animal
+    """
+    def sound(self):
+        """
+        Returns the sound made by a cat.
+        """
+        return "Meow"
