444f0caf483874d0aa4bd7826bc67e8423a92798
Writingway <sendmeat4time@gmail.com>
2026-02-04 10:26:57 +0100

    fix: update Square class docstring and correct size validation

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-Rectangle = __import__('10-square').Rectangle
 """
 Defines a class Square based on Rectangle
 """
 
 
+Rectangle = __import__('10-square').Rectangle
+
+
 class Square(Rectangle):
+    """
+    Represents a square, inherits from Rectangle
+    """
     def __init__(self, size):
         """
         Initializes a Square instance with size
         """
+        self.integer_validator("size", size)
         self.__size = size
-        self.integer_validator("size", self.__size)
         super().__init__(size, size)
 
     def __str__(self):
         """
-        Returns the string representation of the Rectangle
+        Returns the string representation of the Square
         """
         return "[Square] {}/{}".format(self.__size, self.__size)
