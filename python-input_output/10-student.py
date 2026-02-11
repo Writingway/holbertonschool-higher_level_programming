b0168b0059c085d1ff4fd2af7d7639d5ebf06e03
Writingway <sendmeat4time@gmail.com>
2026-02-11 10:13:14 +0100

    fix: update docstring for to_json method in Student class

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a function that returns the dictionary description with simple 
+"""Defines a function that returns the dictionary description with simple
 data structure for JSON serialization of an object"""
 
 
@@ -8,14 +8,14 @@ class Student:
     Represents a student Class.
     """
     def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """
-        If attrs is a list of strings, only attribute names contained in this list must be retrieved.
-        Otherwise, all attributes must be retrieved
+        Retrieves a dictionary representation of a Student instance
         """
         if attrs is None:
             return self.__dict__
