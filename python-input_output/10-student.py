a44fe1db4a22872b90f0244f868ad0950246ef90
Writingway <sendmeat4time@gmail.com>
2026-02-11 10:12:29 +0100

    feat: add Student class with JSON serialization method

---
+++ python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a function that returns the dictionary description with simple 
+data structure for JSON serialization of an object"""
+
+
+class Student:
+    """
+    Represents a student Class.
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        If attrs is a list of strings, only attribute names contained in this list must be retrieved.
+        Otherwise, all attributes must be retrieved
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    new_dict[key] = self.__dict__[key]
+            return new_dict
