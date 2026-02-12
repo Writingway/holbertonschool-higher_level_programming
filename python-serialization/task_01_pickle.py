da67ffc3487aeb4d186e8f1619ba1bb58d8eab43
Writingway <sendmeat4time@gmail.com>
2026-02-12 14:45:07 +0100

    feat: implement CustomObject class with serialization and deserialization methods

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import pickle
+
+
+class CustomObject:
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        if not filename or not filename.endswith('.pkl'):
+            return None
+        with open(filename, "wb") as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        if not filename or not filename.endswith('.pkl'):
+            return None
+        with open(filename, "rb") as file:
+            return pickle.load(file)
