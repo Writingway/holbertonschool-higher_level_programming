e06ab3fd80671df4f5a4ce2143b7ba5042190f8d
Writingway <sendmeat4time@gmail.com>
2026-02-12 14:36:10 +0100

    refactor: replace pickle with JSON for serialization and deserialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -3,16 +3,16 @@
 This module provides functions to serialize and deserialize Python objects
 using the JSON format.
 """
-import pickle
+import json
 
 
 def serialize_and_save_to_file(data, filename):
-    with open(filename, "wb") as file:
-        new_dump = pickle.dump(data, file)
+    with open(filename, "w") as file:
+        new_dump = json.dump(data, file)
         return new_dump
 
 
 def load_and_deserialize(filename):
-    with open(filename, "rb") as file:
-        new_load = pickle.load(file)
+    with open(filename, "r") as file:
+        new_load = json.load(file)
         return new_load
