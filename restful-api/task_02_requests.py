fdaa7aa6520b71e824f94fb9a82724d55ee2e639
Writingway <sendmeat4time@gmail.com>
2026-02-18 11:13:18 +0100

    fix: improve code readability in fetch_and_save_posts function

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -16,22 +16,24 @@ def fetch_and_print_posts():
         print(f"An error occurred: {e}")
 
 
-# if the request was sucessfull, instead of printing titles,
-# structure the data into a list of dictionaries, where each dictionary
-# represents a post with keys and values for id, title, and body.
-# Using Python's csv module, write this data into a CSV file called posts.csv
-# with columns corresponding to the dictionary keys.
 def fetch_and_save_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     try:
         reponses = requests.get(url)
-        reponses.raise_for_status()  # Check if the request was successful
-        posts = reponses.json()  # Parse the JSON response
-        structured_posts = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+        reponses.raise_for_status()
+        posts = reponses.json()
+        structured_posts = [
+            {
+                'id': post.get('id'),
+                'title': post.get('title'),
+                'body': post.get('body')
+            } for post in posts
+        ]
 
         with open('posts.csv', mode='w') as csvfile:
             fieldnames = ['id', 'title', 'body']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
             writer.writerows(structured_posts)
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
