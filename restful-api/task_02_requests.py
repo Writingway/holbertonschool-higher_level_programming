51542dd88be61071e793a39da5537e3445dcb24a
Writingway <sendmeat4time@gmail.com>
2026-02-18 11:20:43 +0100

    fix: move error handling inside response status check in fetch_and_print_posts and fetch_and_save_posts functions

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,28 +6,30 @@ import csv
 def fetch_and_print_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     reponses = requests.get(url)
-    reponses.raise_for_status()  # Check if the request was successful
-    posts = reponses.json()  # Parse the JSON response
     print("Status Code: {}".format(reponses.status_code))
-    for post in posts:
-        print("{}".format(post['title']))
+
+    if reponses.status_code == 200:
+        posts = reponses.json()
+        for post in posts:
+            print("{}".format(post.get('title')))
 
 
 def fetch_and_save_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     reponses = requests.get(url)
-    reponses.raise_for_status()
-    posts = reponses.json()
-    structured_posts = [
-        {
-            'id': post.get('id'),
-            'title': post.get('title'),
-            'body': post.get('body')
-        } for post in posts
-    ]
 
-    with open('posts.csv', mode='w') as csvfile:
-        fieldnames = ['id', 'title', 'body']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(structured_posts)
+    if reponses.status_code == 200:
+        posts = reponses.json()
+        structured_posts = [
+            {
+                'id': post.get('id'),
+                'title': post.get('title'),
+                'body': post.get('body')
+            } for post in posts
+        ]
+
+        with open('posts.csv', mode='w', encoding='utf-8') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(structured_posts)
