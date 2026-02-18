09c1f963dee568ceeb89c938bbd2adca2d4c691d
Writingway <sendmeat4time@gmail.com>
2026-02-18 11:14:43 +0100

    fix: remove redundant error handling in fetch_and_print_posts and fetch_and_save_posts functions

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -5,35 +5,29 @@ import csv
 
 def fetch_and_print_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
-    try:
-        reponses = requests.get(url)
-        reponses.raise_for_status()  # Check if the request was successful
-        posts = reponses.json()  # Parse the JSON response
-        print("Status Code: {}".format(reponses.status_code))
-        for post in posts:
-            print("{}".format(post['title']))
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+    reponses = requests.get(url)
+    reponses.raise_for_status()  # Check if the request was successful
+    posts = reponses.json()  # Parse the JSON response
+    print("Status Code: {}".format(reponses.status_code))
+    for post in posts:
+        print("{}".format(post['title']))
 
 
 def fetch_and_save_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
-    try:
-        reponses = requests.get(url)
-        reponses.raise_for_status()
-        posts = reponses.json()
-        structured_posts = [
-            {
-                'id': post.get('id'),
-                'title': post.get('title'),
-                'body': post.get('body')
-            } for post in posts
-        ]
+    reponses = requests.get(url)
+    reponses.raise_for_status()
+    posts = reponses.json()
+    structured_posts = [
+        {
+            'id': post.get('id'),
+            'title': post.get('title'),
+            'body': post.get('body')
+        } for post in posts
+    ]
 
-        with open('posts.csv', mode='w') as csvfile:
-            fieldnames = ['id', 'title', 'body']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerows(structured_posts)
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+    with open('posts.csv', mode='w') as csvfile:
+        fieldnames = ['id', 'title', 'body']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(structured_posts)
