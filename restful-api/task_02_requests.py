0e632e59a9084f879bdc6554c802e826d4076c32
Writingway <sendmeat4time@gmail.com>
2026-02-18 11:00:38 +0100

    fix: restructure fetch_and_save_posts to write posts data to CSV instead of JSON

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import requests
-import json
+import csv
 
 
 def fetch_and_print_posts():
@@ -16,18 +16,22 @@ def fetch_and_print_posts():
         print(f"An error occurred: {e}")
 
 
+# if the request was sucessfull, instead of printing titles,
+# structure the data into a list of dictionaries, where each dictionary
+# represents a post with keys and values for id, title, and body.
+# Using Python's csv module, write this data into a CSV file called posts.csv
+# with columns corresponding to the dictionary keys.
 def fetch_and_save_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     try:
         reponses = requests.get(url)
-        reponses.raise_for_status()
-        posts = reponses.json()
-        with open("posts.csv", "w") as file:
-            json.dump(posts, file, indent=4)
+        reponses.raise_for_status()  # Check if the request was successful
+        posts = reponses.json()  # Parse the JSON response
+        structured_posts = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+
+        with open('posts.csv', mode='w') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerows(structured_posts)
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
-
-
-if __name__ == "__main__":
-    fetch_and_print_posts()
-    fetch_and_save_posts()
