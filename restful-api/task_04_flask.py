b25012d07905e5cb0cc6140b5cad07c03c0fae28
Writingway <sendmeat4time@gmail.com>
2026-02-18 18:44:16 +0100

    feat: implement user management endpoints for adding and retrieving users

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+from flask import Flask
+from flask import jsonify
+from flask import request
+import json
+
+app = Flask(__name__)
+users = {}
+
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    if request.method == "POST":
+        content = json.loads(request.data)
+        if "username" in content.keys():
+            if content["username"] in users:
+                return jsonify({
+                    "error": "Username already exists"
+                }), 409
+
+            if jsonify(content) is None:
+                return jsonify({"error": "Invalid JSON"}), 400
+
+            user = {
+                "username": content["username"],
+                "name": content["name"],
+                "age": content["age"],
+                "city": content["city"]
+            }
+            users[content["username"]] = user
+            message = "User added successfully"
+
+            return jsonify({
+                "message": message,
+                "user": user
+            }), 201
+        else:
+            return jsonify({"error": "Username is required"}), 400
+
+
+@app.route("/users/<username>")
+def get_user(username):
+    user_info = users.get(username)
+
+    if user_info:
+        return jsonify(user_info)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+
+@app.route("/status")
+def status():
+    return ("OK")
+
+
+@app.route("/data")
+def get_data():
+    usernames = [username for username in users.keys()]
+    return jsonify(usernames)
+
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+
+if __name__ == "__main__":
+    app.run()
