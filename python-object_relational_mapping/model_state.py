54681c1af4fdba8b64da02f2eb80dcb13333ce0f
Writingway <sendmeat4time@gmail.com>
2026-03-06 16:44:09 +0100

    fix: remove charset specification from State model attributes

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -12,5 +12,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-    __table_args__ = {'mysql_charset': 'utf8'}
