a542f9d2a3c7145e2b80b68c53cb7b5a1cb14cf4
Writingway <sendmeat4time@gmail.com>
2026-03-06 13:54:44 +0100

    refactor: improve database connection function formatting and structure

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -3,10 +3,16 @@
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
+
+def connect_to_db():
     """ Filter all states from the database hbtn_0e_0_usa"""
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
                 ORDER BY id ASC")
@@ -15,3 +21,7 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     db.close()
+
+
+if __name__ == "__main__":
+    connect_to_db()
