aa1fd1486c22601d9383047395012d118decd1fd
Writingway <sendmeat4time@gmail.com>
2026-03-06 14:44:11 +0100

    refactor: simplify database connection parameters for improved readability

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -12,12 +12,15 @@ if __name__ == "__main__":
     Filter all states from the database hbtn_0e_0_usa
     that start with N (upper N) and are safe from MySQL injections
     """
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        password=sys.argv[2],
-        database=sys.argv[3]
+        user=user,
+        password=password,
+        db=database
     )
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%'\
