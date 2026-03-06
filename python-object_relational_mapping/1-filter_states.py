fb98832fd0d21675a0ab283c239dea942f58e9c6
Writingway <sendmeat4time@gmail.com>
2026-03-06 12:59:18 +0100

    fix: correct SQL query to filter states starting with 'N' and order by id

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE REGEXP_LIKE(name, '^[N]', 'i')")
+    cur.execute("SELECT * FROM states WHERE REGEXP_LIKE(name, '^[N]') ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
