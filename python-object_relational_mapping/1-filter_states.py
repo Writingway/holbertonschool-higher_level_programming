5187d8183b69e31723cb97425e0cb74ce7e8fed9
Writingway <sendmeat4time@gmail.com>
2026-03-06 14:51:51 +0100

    refactor: improve database connection parameter handling for clarity

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,12 +8,15 @@ import sys
 
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        password=sys.argv[2],
-        database=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database
     )
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
