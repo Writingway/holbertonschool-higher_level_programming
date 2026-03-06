2fefb8d1a108543ad546f294a85400ea1fd79b99
Writingway <sendmeat4time@gmail.com>
2026-03-06 15:27:49 +0100

    fix: refactor SQL query execution to use a variable for state filtering

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,6 +4,8 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    states = sys.argv[4]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306, user=sys.argv[1],
@@ -11,8 +13,10 @@ if __name__ == "__main__":
         database=sys.argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
-                (sys.argv[4],))
+    cur.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
+        (states, )
+    )
     rows = cur.fetchall()
     for row in rows:
         print(row)
