681ca18015f74b27dfef5970fdac5d2f5b73e049
Writingway <sendmeat4time@gmail.com>
2026-03-06 15:45:10 +0100

    fix: refactor variable assignments for database connection parameters in 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -4,15 +4,20 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    states = sys.argv[4]
+
     db = MySQLdb.connect(
         host="localhost",
-        port=3306, user=sys.argv[1],
-        password=sys.argv[2],
-        database=sys.argv[3]
+        port=3306, user=username,
+        password=password,
+        database=database,
     )
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
-                .format(sys.argv[4]))
+                .format(states))
     rows = cur.fetchall()
     for row in rows:
         print(row)
