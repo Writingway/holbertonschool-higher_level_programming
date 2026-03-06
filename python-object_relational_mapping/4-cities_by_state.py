06705ae0d07fbeff8146047ec17bf6faf2cacaab
Writingway <sendmeat4time@gmail.com>
2026-03-06 15:37:17 +0100

    fix: ensure database connection is properly closed with a newline at the end of the file

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" lists all cities from the database hbtn_0e_4_usa """
+"""lists all cities from the database hbtn_0e_4_usa"""
+
 import MySQLdb
 import sys
 
@@ -7,6 +8,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -14,16 +16,19 @@ if __name__ == "__main__":
         passwd=password,
         db=database
     )
-
     cur = db.cursor()
+
     cur.execute(
         "SELECT cities.id, cities.name, states.name "
         "FROM cities "
         "JOIN states ON cities.state_id = states.id "
         "ORDER BY cities.id ASC"
     )
+
     rows = cur.fetchall()
+
     for row in rows:
         print(row)
+
     cur.close()
     db.close()
