0840e891adbef61f6b973bd3e84520b74e0b50ae
Writingway <sendmeat4time@gmail.com>
2026-03-06 15:35:08 +0100

    fix: improve SQL query formatting by adding missing newline for readability

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -20,7 +20,8 @@ if __name__ == "__main__":
         "SELECT cities.id, cities.name, states.name "
         "FROM cities "
         "JOIN states ON cities.state_id = states.id "
-        "ORDER BY cities.id ASC")
+        "ORDER BY cities.id ASC"
+    )
     rows = cur.fetchall()
     for row in rows:
         print(row)
