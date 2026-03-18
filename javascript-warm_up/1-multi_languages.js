efe9487dfdd5765f7f77dd550fa815b7ec9e770f
Writingway <sendmeat4time@gmail.com>
2026-03-18 17:45:28 +0100

    fix: separate variable declarations in multi_languages script

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const first = 'C is Fun', second = 'Python is cool', third = 'JavaScript is amazing';
+const first = 'C is Fun';
+const second = 'Python is cool';
+const third = 'JavaScript is amazing';
+
 console.log(first + '\n' + second + '\n' + third);
