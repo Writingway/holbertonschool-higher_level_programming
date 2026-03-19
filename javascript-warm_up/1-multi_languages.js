366afa54021e88cb2840d220891271f2b7cbe15c
Writingway <sendmeat4time@gmail.com>
2026-03-19 09:32:04 +0100

    fix: separate console.log statements in multi_languages script

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -2,5 +2,6 @@
 const first = 'C is Fun';
 const second = 'Python is cool';
 const third = 'JavaScript is amazing';
-
-console.log(first + '\n' + second + '\n' + third);
+console.log(first);
+console.log(second);
+console.log(third);
