c04da69278bf0a0934c6d6fe60b939e5ff51a59e
Writingway <sendmeat4time@gmail.com>
2026-03-20 11:11:49 +0100

    fix: update argument checking logic in 2-arguments script

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
+const argv = process.argv.slice(2)
 
-if (!argv[2]) {
+if (argv.length === 0) {
   console.log('No argument');
-} else {
+} else if (argv.length === 1) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
