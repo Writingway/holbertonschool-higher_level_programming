6053ac17741738f6172d761a155c536e9ec6eba9
Writingway <sendmeat4time@gmail.com>
2026-03-20 13:43:50 +0100

    fix: correct indentation and variable reference in 7-multi_c script

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@ const languages = ['C is fun'];
 const x = parseInt(process.argv[2]);
 
 if (isNaN(x)) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < x; i++) {
-        console.log(speak[0]);
-    }
+  for (let i = 0; i < x; i++) {
+    console.log(languages[0]);
+  }
 }
